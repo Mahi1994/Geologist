9596f947448700f1bb739c977f81357529ef0389
Mahi1994 <74986358+Mahi1994@users.noreply.github.com>
2021-01-23 22:13:46 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -15,6 +15,7 @@ class Rubber{
       push();
       translate(this.body.position.x, this.body.position.y);
       rotate(angle);
+      fill("blue");
       ellipseMode(CENTER);
       ellipse(this.image, 0, 0, this.radius, this.radius);
       pop();
