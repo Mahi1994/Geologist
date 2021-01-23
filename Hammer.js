666809bbb441ef34d96fbbd8be9bec57d664c11b
Mahi1994 <74986358+Mahi1994@users.noreply.github.com>
2021-01-23 22:15:48 +0530

    Update Hammer.js

--- Hammer.js
+++ Hammer.js
@@ -1,29 +1,29 @@
 class Hammer {
-    constructor(x,y,angle){
-        var options = {
-          density : 2,
-          friction : 1.0,
-          restitution :0.5
-      }
-      this.body =  Bodies.rectangle(x,y,150,40,options);
+    constructor(x, y) {
+      var options = {
+        'density':2,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
       this.width = 150;
       this.height = 40;
-      Matter.Body.setAngle(this.body, angle);
-      World.add(world,this.body);
-    }
-  
-    display() {
-      var angle = this.body.angle;
+      World.add(world, this.body);
+    };
+    display(){
       var pos = this.body.position;
       pos.x = mouseX;
       pos.y = mouseY;
+      var angle = this.body.angle;
+  
       push();
-      fill("yellow")
       translate(pos.x, pos.y);
       rotate(angle);
-      rectMode(CENTER);
-      rect(0,0,this.width,this.height);
+      strokeWeight(3);
+      stroke('white')
+      fill('orange')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
       pop();
-      
-    }
-  }
+    };
+  };
