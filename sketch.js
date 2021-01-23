0b51f256faa74ced2d18600a32b5d5c28403adc3
Mahi1994 <74986358+Mahi1994@users.noreply.github.com>
2021-01-23 22:19:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,16 +19,23 @@ function setup() {
 	world = engine.world;
      
 	//Create the Bodies Here.
-	hammer = new Hammer(100,100,PI/3);
-	ground = new Ground(400,680,800,15)
-	rubber = new Rubber(400,350,50);
-	sand1 = new Sand(100,350,20)
-	sand2 = new Sand(150,350,20)
-	sand3 = new Sand(200,350,20)
-	sand4 = new Sand(250,350,20)
-	sand5 = new Sand(350,350,20)
-	iron = new Iron(600,350,100,70)
-	stone = new Stone(50,350,200,200)
+    hammer = new Hammer(10,100);
+    ground = new Ground(600,height,1200,20)
+    stone = new Stone(700,320,100,100);
+    iron = new Iron(300,350);
+    rubber=new Rubber(900,450,70);
+
+    sand1 = new Sand(505,450,10);
+    sand2 = new Sand(510,450,10);
+    sand3 = new Sand(515,450,10);
+    sand4 = new Sand(520,450,10);
+    sand5 = new Sand(525,450,10);
+    sand6 = new Sand(515,445,10);
+    sand7 = new Sand(520,445,10);
+    sand8 = new Sand(525,445,10);
+    sand9 = new Sand(530,445,10);
+    sand10 = new Sand(535,445,10);
+
 	Engine.run(engine);
   
 }
@@ -38,16 +45,23 @@ function draw() {
   background("lightblue");
   Engine.update(engine);
   rectMode(CENTER);
-  ground.display();
-  hammer.display();
-  rubber.display();
-  sand1.display();
-  sand2.display();
-  sand3.display();
-  sand4.display();
-  sand5.display();
-  iron.display();
-  stone.display();
+    ground.display();
+    stone.display();
+    iron.display();
+    rubber.display();
+
+    hammer.display();
+
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
 }
 
 
